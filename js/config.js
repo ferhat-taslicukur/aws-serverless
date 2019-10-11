e667886b1dfec8db0a2d420999b8da3225aa0451
EC2 Default User <ec2-user@ip-172-31-2-203.eu-west-1.compute.internal>
2019-10-11 16:02:49 +0000

    config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-west-1_7Ehh1eNWy', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3cl1vv024nh23jtp29b070gpd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3cl1vv024nh23jtp29b070gpd6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
